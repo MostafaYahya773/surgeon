891d1fcb2ccb4a87d560f69d5e8755d5fe6b94fd
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2025-11-21 05:13:29 +0200

    fix: add demo email

--- src/app/auth/Login/page.tsx
+++ src/app/auth/Login/page.tsx
@@ -67,82 +67,86 @@ export default function Login() {
 
   return (
     <div className="flex justify-center items-center mx-auto h-screen">
-      <div className="dark:bg-slate-800 w-fit h-fit rounded-lg p-6">
-        <div className="w-full flex flex-col gap-10">
-          <div className="title flex justify-between items-center">
-            <Image
-              src="/nav/SurgeonLogo.png"
-              alt="logo"
-              width={25}
-              height={25}
-            />
-            <Link
-              href="/auth/SignUp"
-              className="text-blue-600 dark:text-white text-[15px] border border-slate-600 py-[2px] px-5 rounded-lg dark:hover:bg-white dark:hover:text-blue-400 transition-all duration-300"
-            >
-              Register
-            </Link>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
-              <h3 className="text-2xl font-bold text-blue-400">
-                Log in To Your Account
-              </h3>
-              <p className="dark:text-secondary text-[14px]">
-                Login with your data that you entered during registration
-              </p>
-            </div>
-            <form onSubmit={formik.handleSubmit}>
-              <div className="flex flex-col gap-4">
-                {LoginInputs.map((input, index) => (
-                  <div className="input relative" key={index}>
-                    <input
-                      className="w-full p-2 px-8 rounded-lg outline-none text-[15px] border dark:border-slate-700 focus:border-slate-600 dark:text-white dark:bg-slate-800"
-                      type={input.type}
-                      name={input.name}
-                      placeholder={input.placeHolder}
-                      value={formik.values[input.name as keyof Auth]}
-                      onChange={formik.handleChange}
-                    />
-                    <span className="absolute left-0 top-1/4 px-2">
-                      {input.icon}
-                    </span>
-                    {formik.errors[input?.name as keyof Auth] &&
-                      (formik.submitCount > 0 ||
-                        formik.values[input?.name as keyof Auth] !== '') && (
-                        <div className="text-red-400 text-[14px] mt-1">
-                          {formik.errors[input?.name as keyof Auth]}
-                        </div>
-                      )}
-                  </div>
-                ))}
-                <Link
-                  className=" w-fit text-blue-400 text-[14px] px-2 hover:underline ms-auto"
-                  href="/auth/ConfirmEmail"
-                >
-                  Forget password?
-                </Link>
-              </div>
-              <button
-                type="submit"
-                disabled={formik.isSubmitting}
-                className="w-full p-2 bg-blue-500 rounded-lg mt-2 text-white hover:bg-blue-400 disabled:opacity-50"
+      <div className="flex flex-col gap-5">
+        <div className="dark:bg-slate-800 w-fit h-fit rounded-lg p-6">
+          <div className="w-full flex flex-col gap-10">
+            <div className="title flex justify-between items-center">
+              <Image
+                src="/nav/SurgeonLogo.png"
+                alt="logo"
+                width={25}
+                height={25}
+              />
+              <Link
+                href="/auth/SignUp"
+                className="text-blue-600 dark:text-white text-[15px] border border-slate-600 py-[2px] px-5 rounded-lg dark:hover:bg-white dark:hover:text-blue-400 transition-all duration-300"
               >
-                {formik.isSubmitting ? <LoadingSave /> : 'Login'}
-              </button>
-            </form>
+                Register
+              </Link>
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-1">
+                <h3 className="text-2xl font-bold text-blue-400">
+                  Log in To Your Account
+                </h3>
+                <p className="dark:text-secondary text-[14px]">
+                  Login with your data that you entered during registration
+                </p>
+              </div>
+              <form onSubmit={formik.handleSubmit}>
+                <div className="flex flex-col gap-4">
+                  {LoginInputs.map((input, index) => (
+                    <div className="input relative" key={index}>
+                      <input
+                        className="w-full p-2 px-8 rounded-lg outline-none text-[15px] border dark:border-slate-700 focus:border-slate-600 dark:text-white dark:bg-slate-800"
+                        type={input.type}
+                        name={input.name}
+                        placeholder={input.placeHolder}
+                        value={formik.values[input.name as keyof Auth]}
+                        onChange={formik.handleChange}
+                      />
+                      <span className="absolute left-0 top-1/4 px-2">
+                        {input.icon}
+                      </span>
+                      {formik.errors[input?.name as keyof Auth] &&
+                        (formik.submitCount > 0 ||
+                          formik.values[input?.name as keyof Auth] !== '') && (
+                          <div className="text-red-400 text-[14px] mt-1">
+                            {formik.errors[input?.name as keyof Auth]}
+                          </div>
+                        )}
+                    </div>
+                  ))}
+                  <Link
+                    className=" w-fit text-blue-400 text-[14px] px-2 hover:underline ms-auto"
+                    href="/auth/ConfirmEmail"
+                  >
+                    Forget password?
+                  </Link>
+                </div>
+                <button
+                  type="submit"
+                  disabled={formik.isSubmitting}
+                  className="w-full p-2 bg-blue-500 rounded-lg mt-2 text-white hover:bg-blue-400 disabled:opacity-50"
+                >
+                  {formik.isSubmitting ? <LoadingSave /> : 'Login'}
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white dark:bg-slate-700 w-fit mx-auto mt-5 h-fit p-2 rounded-lg">
-        <p>
-          DemoEmail :
-          <span className="text-secondary text-[14px]">test123@gmail.com</span>
-        </p>
-        <p>
-          Password :
-          <span className="text-secondary text-[14px]">Test12345</span>
-        </p>
+        <div className="bg-white dark:bg-slate-700 w-fit mx-auto h-fit p-2 rounded-lg">
+          <p className="dark:text-blue-500">
+            DemoEmail :{' '}
+            <span className="text-secondary text-[14px]">
+              test123@gmail.com
+            </span>
+          </p>
+          <p className="dark:text-blue-500">
+            Password :{' '}
+            <span className="text-secondary text-[14px]">Test12345</span>
+          </p>
+        </div>
       </div>
     </div>
   );
